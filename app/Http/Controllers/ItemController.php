a10671300f96e2df5f2f5a203b3f22615d3cdca9
nekkiChan <nekkiChan@users.noreply.github.com>
2023-09-17 11:40:23 +0900

    修正：コードの整頓

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,229 +2,112 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Item;
-use App\Models\ItemType; // ItemType モデルを追加
+use Illuminate\Support\Facades\Gate;
+use App\Services\ItemService;
 
 class ItemController extends Controller
 {
-    // コンストラクターでモデルのインスタンスを作成する必要はありません
+    protected $itemService;
 
-    /**
-     * 商品一覧
-     */
+    public function __construct(ItemService $itemService)
+    {
+        $this->itemService = $itemService;
+    }
+
     public function index(Request $request)
     {
-        // ユーザー情報取得
         $user = Auth::user();
-
-        // ページ情報
         $page = 'index';
-
-        // 商品一覧取得
-        // with メソッドを使って ItemType モデルを事前に読み込む
-        $items = Item::with('itemType')->where('status', '!=', 'delete')->get();
-        // 種別ID→種別名
-        $items->transform(function ($item) {
-            $item->type = $item->itemType->name;
-            return $item;
-        });
+        $items = $this->itemService->getActiveItems();
 
         return view('item.index', compact('user', 'items', 'page'));
     }
 
-    /**
-     * アーカイブ一覧
-     */
     public function archive(Request $request)
     {
-        // ユーザー情報取得
         $user = Auth::user();
-
-        // ページ情報
         $page = 'archive';
 
         if (Gate::allows('view-user', $user)) {
-            // 商品一覧取得
-            // with メソッドを使って ItemType モデルを事前に読み込む
-            $items = Item::with('itemType')->where('status', '=', 'delete')->get();
-            $items->transform(function ($item) {
-                $item->type = $item->itemType->name;
-                return $item;
-            });
-
-            return view('item.archive', compact('user', 'items', 'page'));
-
+            $items = $this->itemService->getDeletedItems();
         } else {
-            // 一般アカウントの場合
             return redirect('/items/archive');
         }
+
+        return view('item.archive', compact('user', 'items', 'page'));
     }
 
-    /**
-     * 商品検索
-     */
     public function search(Request $request)
     {
-        // ユーザー情報取得
         $user = Auth::user();
-
-        // ページ取得
         $page = $request->input('page');
-        // 商品のステータス
         $status = $request->input('status');
-        // ステータスに即した商品一覧
-        $items = Item::where('status', '=', "$status");
-        // 検索タイプ
-        $search_type = $request->input('search_type');
-        // 検索ワードに基づいて商品を検索
         $keyword = $request->input('keyword');
+        $searchType = $request->input('search_type');
 
-        // 検索ワードが空欄の場合、絞り込みはしない
-        if (is_null($keyword)) {
-            $items = $items->get();
-        } else if ($search_type == 'type') {
-            // 種別検索
-            $items = $items->whereHas(
-                'itemType',
-                function ($query) use ($keyword) {
-                    $query->where('name', 'like', "%$keyword%");
-                }
-            )->get();
-        } else if ($search_type == 'name' || $search_type == 'detail') {
-            // 名前検索・詳細検索
-            $items = $items->where(
-                function ($query) use ($keyword, $search_type) {
-                    $query->where($search_type, 'like', "%$keyword%");
-                }
-            )->get();
-        } else {
-            // 全体検索
-            $items = $items->where(function ($query) use ($keyword) {
-                $query->where('name', 'like', "%$keyword%")
-                    ->orWhereHas('itemType', function ($subquery) use ($keyword) {
-                        $subquery->where('name', 'like', "%$keyword%");
-                    })
-                    ->orWhere('detail', 'like', "%$keyword%");
-            })
-                ->get();
-
-        }
-
-        // 種別ID→種別名
-        $items->transform(function ($item) {
-            $item->type = $item->itemType->name;
-            return $item;
-        });
+        $items = $this->itemService->searchItems($status, $searchType, $keyword);
 
         return view("item.$page", compact('user', 'items', 'page'));
     }
 
-
-    /**
-     * 商品登録
-     */
     public function add(Request $request)
     {
         $user = Auth::user();
 
         if (Gate::allows('view-user', $user)) {
-            // 管理者アカウントの場合
-            // POSTリクエストのとき
             if ($request->isMethod('post')) {
-                // バリデーション
                 $this->validate($request, [
                     'name' => 'required|max:100',
                     'type' => 'required|max:100',
                 ]);
 
-                // 種別登録
-                $itemType = ItemType::firstOrCreate(['name' => $request->type]);
-
-                // 商品登録
-                Item::create([
-                    'user_id' => Auth::user()->id,
-                    'name' => $request->name,
-                    'type' => $itemType->id,
-                    'detail' => $request->detail,
-                ]);
+                $this->itemService->addItem($request->all());
 
                 return redirect('/items');
             }
 
             return view('item.add');
         } else {
-            // 一般アカウントの場合
             return redirect('/items');
         }
     }
 
-    /**
-     * 商品編集
-     */
     public function edit(Request $request)
     {
-        // ユーザー取得
         $user = Auth::user();
-        // ページ取得
         $page = $request->input('page');
-        // 商品のステータス取得
         $status = $request->input('status');
-        $items = Item::where('status', '=', "$status")->get();
+        $items = $this->itemService->getItemsByStatus($status);
 
         if (Gate::allows('view-user', $user)) {
-            // 管理者アカウントの場合
-            // POSTリクエストのとき
             if ($request->isMethod('post')) {
-                // バリデーション
                 $this->validate($request, [
                     'name' => 'required|max:100',
                     'type' => 'required|max:100',
                 ]);
 
-                // 種別登録
-                $itemType = ItemType::firstOrCreate(['name' => $request->type]);
+                $this->itemService->updateItem($request->all());
 
-                // データを更新
-                $item = Item::find($request->id);
-                $item->update([
-                    'name' => $request->name,
-                    'type' => $itemType->id,
-                    'detail' => $request->detail
-                ]);
-
-                // 種別ID→種別名
-                $items->transform(function ($item) {
-                    $item->type = $item->itemType->name;
-                    return $item;
-                });
+                $items = $this->itemService->getItemsByStatus($status);
 
                 return view("item.$page", compact('user', 'items'));
-            } else if ($request->isMethod('get')) {
-                // GETリクエストのとき
-                $items = Item::with('itemType')->get();
-                $item = $items->find($request->item_id);
-                $item->type = $item->itemType->name;
+            } elseif ($request->isMethod('get')) {
+                $item = $this->itemService->getItemById($request->item_id);
+
                 return view('item.edit', compact('item'));
             }
         } else {
-            // 一般アカウントの場合
             return redirect('/items');
         }
     }
 
     public function convertStatus(Request $request)
     {
-        // 商品取得
-        $item = new Item;
-        $item->convertStatus($request->item_id);
-
-        // ページ取得
+        $this->itemService->convertItemStatus($request->item_id);
         $page = $request->page;
 
         return redirect(route("items.$page"));
     }
-
 }
---
+++ app/Services/ItemService.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Item;
+use App\Models\ItemType;
+
+class ItemService
+{
+    public function getActiveItems()
+    {
+        return Item::with('itemType')->where('status', '!=', 'delete')->get();
+    }
+
+    public function getDeletedItems()
+    {
+        return Item::with('itemType')->where('status', '=', 'delete')->get();
+    }
+
+    public function searchItems($status, $searchType, $keyword)
+    {
+        $items = Item::where('status', $status);
+
+        if (!is_null($keyword)) {
+            $items = $this->applySearchCriteria($items, $searchType, $keyword);
+        }
+
+        return $items->get();
+    }
+
+    public function addItem($data)
+    {
+        $itemType = ItemType::firstOrCreate(['name' => $data['type']]);
+        Item::create([
+            'user_id' => auth()->user()->id,
+            'name' => $data['name'],
+            'type' => $itemType->id,
+            'detail' => $data['detail'],
+        ]);
+    }
+
+    public function updateItem($data)
+    {
+        $itemType = ItemType::firstOrCreate(['name' => $data['type']]);
+        $item = Item::find($data['id']);
+        $item->update([
+            'name' => $data['name'],
+            'type' => $itemType->id,
+            'detail' => $data['detail'],
+        ]);
+    }
+
+    public function getItemById($itemId)
+    {
+        return Item::with('itemType')->find($itemId);
+    }
+
+    public function convertItemStatus($itemId)
+    {
+        $item = Item::find($itemId);
+        $item->convertStatus();
+    }
+
+    private function applySearchCriteria($query, $searchType, $keyword)
+    {
+        if ($searchType == 'type') {
+            $query->whereHas(
+                'itemType',
+                function ($query) use ($keyword) {
+                    $query->where('name', 'like', "%$keyword%");
+                }
+            );
+        } elseif (in_array($searchType, ['name', 'detail'])) {
+            $query->where($searchType, 'like', "%$keyword%");
+        } else {
+            $query->where(function ($query) use ($keyword) {
+                $query->where('name', 'like', "%$keyword%")
+                    ->orWhereHas('itemType', function ($subquery) use ($keyword) {
+                        $subquery->where('name', 'like', "%$keyword%");
+                    })
+                    ->orWhere('detail', 'like', "%$keyword%");
+            });
+        }
+
+        return $query;
+    }
+}
